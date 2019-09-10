6ad8667c4fba75f2ec8bb9ae3bab893280851641
FreakyCoder <kurayogun@gmail.com>
2019-09-10 23:16:15 +0300

    Refactor is done and also ripple onPress is added

--- example/lib/src/components/CircularCard/CircularCard.js
+++ example/lib/src/components/CircularCard/CircularCard.js
@@ -3,72 +3,90 @@ import PropTypes from "prop-types";
 import { Text, View, Image } from "react-native";
 import Androw from "react-native-androw";
 import Icon from "react-native-dynamic-vector-icons";
-import { ScreenWidth, ScreenHeight } from "@freakycoder/react-native-helpers";
+import Ripple from "react-native-material-ripple";
+import styles, { _container, _shadowStyle } from "./CircularCard.style";
+import { ScreenWidth } from "@freakycoder/react-native-helpers";
 
 const CircularCard = props => {
-  const { title } = props;
+  const {
+    width,
+    title,
+    height,
+    subtitle,
+    priceText,
+    imageStyle,
+    titleStyle,
+    priceStyle,
+    shadowColor,
+    shadowStyle,
+    imageSource,
+    subtitleStyle,
+    backgroundColor
+  } = props;
   return (
-    <Androw
-      style={{
-        shadowRadius: 8,
-        shadowColor: "#000",
-        shadowOpacity: 0.1,
-        shadowOffset: {
-          width: 4,
-          height: 4
-        }
-      }}
-    >
-      <View
-        style={{
-          height: 125,
-          borderTopEndRadius: 30,
-          width: ScreenWidth * 0.9,
-          borderBottomEndRadius: 30,
-          borderTopStartRadius: 100,
-          backgroundColor: "#fcfcfc",
-          borderBottomStartRadius: 100
-        }}
+    <Androw style={shadowStyle || _shadowStyle(shadowColor)}>
+      <Ripple
+        rippleColor="#f5cbdc"
+        rippleDuration={1250}
+        rippleContainerBorderRadius={30}
+        style={_container(height, width, backgroundColor)}
+        {...props}
       >
-        <View style={{ flexDirection: "row" }}>
+        <View style={styles.containerGlue}>
           <Image
-            style={{ width: 125, height: 125, borderRadius: 75 }}
-            source={{
-              uri:
-                "https://images.unsplash.com/photo-1520763185298-1b434c919102?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1189&q=80"
-            }}
+            style={imageStyle || styles.imageStyle}
+            source={
+              imageSource || {
+                uri:
+                  "https://images.unsplash.com/photo-1520763185298-1b434c919102?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1189&q=80"
+              }
+            }
           />
-          <View style={{ margin: 16, width: ScreenWidth * 0.4 }}>
-            <Text style={{ fontSize: 20, color: "#313032" }}>Gerbera</Text>
+          <View style={styles.textContainer}>
+            <Text style={titleStyle || styles.titleStyle}>{title}</Text>
             <View style={{ marginTop: 8 }}>
-              <Text
-                style={{ fontSize: 10, color: "#ddddde", fontWeight: "700" }}
-              >
-                Gerbera's colors and form perfectly c onvey the best and most
-                positive feelings.
+              <Text style={subtitleStyle || styles.subtitleStyle}>
+                {subtitle}
               </Text>
             </View>
           </View>
-          <View style={{ position: "absolute", top: 16, right: 16 }}>
-            <Icon name="heart" type="AntDesign" size={22} color="#f5cbdc" />
+          <View style={styles.iconContainer}>
+            <Icon
+              name="heart"
+              type="AntDesign"
+              size={22}
+              color="#f5cbdc"
+              {...props}
+            />
           </View>
-          <View style={{ position: "absolute", bottom: 16, right: 16 }}>
-            <Text style={{ fontSize: 20, fontWeight: "600", color: "#414045" }}>
-              $1
-            </Text>
+          <View style={styles.priceTextContainer}>
+            <Text style={priceStyle || styles.priceTextStyle}>{priceText}</Text>
           </View>
         </View>
-      </View>
+      </Ripple>
     </Androw>
   );
 };
 
 CircularCard.propTypes = {
-  example: PropTypes.number
+  width: PropTypes.number,
+  title: PropTypes.string,
+  height: PropTypes.number,
+  subtitle: PropTypes.string,
+  priceText: PropTypes.string,
+  shadowColor: PropTypes.string,
+  backgroundColor: PropTypes.string
 };
 
 CircularCard.defaultProps = {
-  example: 5
+  shadowColor: "#000",
+  title: "Gerbera",
+  subtitle:
+    "Gerbera's colors and form perfectly c onvey the best and most positive feelings.",
+  height: 125,
+  width: ScreenWidth * 0.9,
+  backgroundColor: "#fcfcfc",
+  priceText: "$1"
 };
 
 export default CircularCard;
